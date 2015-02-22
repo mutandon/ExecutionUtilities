dfa1d8223c8ebc921d1ababeba572583b3beb69c
Davide <mutandon@users.noreply.github.com>
2015-02-22 14:39:20 +0100

    Updated javascript batch example

--- batch_example.js
+++ batch_example.js
@@ -2,9 +2,12 @@
  * Example javascript to be executed in console command batch
  */
 
+// The following two lines are mandatories, the variable "commands" must be present
+// and filled with the string containing the commands to be run. 
 var ArrayList = Java.type('java.util.ArrayList');
 var commands = new ArrayList();
-
+ 
+// Here put your code and fill the commands array
 for (i = 0; i < 10; i++) {
     commands.add("exec (Dummy -w dummyparam)"); 
 }
