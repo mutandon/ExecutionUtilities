7a7b3ca04da1ea2c97b6f8923a82727f0df49ad1
Davide <mutandon@users.noreply.github.com>
2015-02-23 11:05:41 +0100

    Update batch_example.js

--- batch_example.js
+++ batch_example.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Davide Mottin <mottin@disi.unitn.eu>
+ * Copyright (C) 2015 Davide Mottin <mottin@disi.unitn.eu>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
